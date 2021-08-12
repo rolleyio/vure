9137591be139ea0f5e194696dfc763f5acc3b268
Matthew Rollinson <matt@rolley.io>
2021-08-12 23:59:36 +0100

    upgrade deps

--- src/index.ts
+++ src/index.ts
@@ -28,61 +28,65 @@ export function defineVureConfig(options: VureConfig) {
   return options;
 }
 
+// Currently don't need the Vue app to inject so basic function
+export function install(options: VureConfig) {
+  const { name, features, config } = options;
+
+  initializeFirebaseApp(config, name);
+
+  if (features.analytics) {
+    initializeAnalytics();
+  }
+
+  if (features.appCheck) {
+    const { provider, isTokenAutoRefreshEnabled } = features.appCheck;
+
+    if (provider) {
+      initializeAppCheck(provider, isTokenAutoRefreshEnabled);
+    } else {
+      throw new Error(
+        'You need to provide a provider in features.appCheck config to enable appCheck',
+      );
+    }
+  }
+
+  if (features.auth) {
+    initializeAuth(features.auth.emulator);
+  }
+
+  if (features.firestore) {
+    initializeFirestore(features.firestore.emulator);
+  }
+
+  if (features.functions) {
+    const { regionOrCustomDomain, emulator: fnsEmulator } =
+      features.functions;
+
+    initializeFunctions(regionOrCustomDomain, fnsEmulator);
+  }
+
+  if (features.messaging) {
+    initializeMessaging();
+  }
+
+  if (features.performance) {
+    initializePerformance();
+  }
+
+  if (features.remoteConfig) {
+    initializeRemoteConfig();
+  }
+
+  if (features.storage) {
+    const { bucketUrl, emulator: storageEmulator } = features.storage;
+
+    initializeStorage(bucketUrl, storageEmulator);
+  }
+}
+
+// Have a basic vue plugin, just in case
 export default {
   install(_: App, options: VureConfig) {
-    const { name, features, config } = options;
-
-    initializeFirebaseApp(config, name);
-
-    if (features.analytics) {
-      initializeAnalytics();
-    }
-
-    if (features.appCheck) {
-      const { provider, isTokenAutoRefreshEnabled } =
-        features.appCheck;
-
-      if (provider) {
-        initializeAppCheck(provider, isTokenAutoRefreshEnabled);
-      } else {
-        throw new Error(
-          'You need to provide a provider in features.appCheck config to enable appCheck',
-        );
-      }
-    }
-
-    if (features.auth) {
-      initializeAuth(features.auth.emulator);
-    }
-
-    if (features.firestore) {
-      initializeFirestore(features.firestore.emulator);
-    }
-
-    if (features.functions) {
-      const { regionOrCustomDomain, emulator: fnsEmulator } =
-        features.functions;
-
-      initializeFunctions(regionOrCustomDomain, fnsEmulator);
-    }
-
-    if (features.messaging) {
-      initializeMessaging();
-    }
-
-    if (features.performance) {
-      initializePerformance();
-    }
-
-    if (features.remoteConfig) {
-      initializeRemoteConfig();
-    }
-
-    if (features.storage) {
-      const { bucketUrl, emulator: storageEmulator } =
-        features.storage;
-
-      initializeStorage(bucketUrl, storageEmulator);
-    }
+    install(options);
   },
 };
